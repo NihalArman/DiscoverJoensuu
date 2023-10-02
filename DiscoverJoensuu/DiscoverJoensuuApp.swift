7c14b34f36ccdc25d969c98e0b44fb721267351a
Arman Nihal <armansaliknihal@gmail.com>
2023-10-02 19:51:42 +0300

    Initial Commit

---
+++ DiscoverJoensuu/DiscoverJoensuuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverJoensuuApp.swift
+//  DiscoverJoensuu
+//
+//  Created by Nihal Arman on 2.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverJoensuuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
