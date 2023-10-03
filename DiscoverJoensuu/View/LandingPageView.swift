07f1212847b2a591026d8db69efab3d2208709e0
Arman Nihal <armansaliknihal@gmail.com>
2023-10-03 20:39:21 +0300

    Introduce a landing page to the application

---
+++ DiscoverJoensuu/View/LandingPageView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeScreenView.swift
+//  DiscoverJoensuu
+//
+//  Created by Nihal Arman on 3.10.2023.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text("Landing Page view")
+            .font(.system(size: 40))
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
+}
